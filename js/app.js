50bad02868bb5a277651d6bf504ed088e55ff5f4
softcactus <cactus@cactusworks.net>
2015-05-01 15:28:00 -0700

    lots of comments

--- js/app.js
+++ js/app.js
@@ -1,4 +1,7 @@
 
+// Patrick's front end javascript project from Udacity.
+// See https://github.com/udacity/frontend-nanodegree-arcade-game
+// for original files.
 
 // Enemies our player must avoid
 var Enemy = function(x,y,speed) {
@@ -37,7 +40,6 @@ Enemy.prototype.render = function() {
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
-
 var Player = function () {
     this.sprite = 'images/char-horn-girl.png';
     this.x = 200;
@@ -45,15 +47,22 @@ var Player = function () {
     this.speed = 20;
 }
 
+// Required method that updateEntities from the engine file
+// runs.
 Player.prototype.update = function (dt) {
     this.x*dt;
     this.y*dt;
 }
 
+
+// Method that renderEntities from engine.js runs to render
+// the player.
 Player.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 }
 
+// Takes input from the keyup document listener and runs a
+// simple if else function to determine movment direction.
 Player.prototype.handleInput = function(key) {
     if (key === 'left' && this.x > 0) {
         this.x -= 101;
@@ -62,7 +71,7 @@ Player.prototype.handleInput = function(key) {
     } else if (key === 'up' && this.y > 0) {
         this.y -= 83;
     } else if (key === 'down' && this.y < 400) {
-        this.y += 50;
+        this.y += 83;
     }
 };
 
@@ -73,8 +82,12 @@ Player.prototype.handleInput = function(key) {
 
 
 var player = new Player();
-var enemy1 = new Enemy(-100,60,400)
-var allEnemies = [enemy1]
+var enemy10 = new Enemy(-100,60,400)
+var enemy11 = new Enemy(200,60,400)
+var enemy2 = new Enemy(50,140,350)
+var enemy30 = new Enemy(100,220,300)
+var enemy31 = new Enemy(400,220,300)
+var allEnemies = [enemy10,enemy11,enemy2,enemy30,enemy31]
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
